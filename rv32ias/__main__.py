590785a89e84a8acae902f4a66afc2a06bc027be
Ri Teii <teiiri@outlook.com>
2024-03-22 17:05:53 +0000

    🔄 Move main function from `main.py` to `__main__.py`

--- rv32ias/__main__.py
+++ rv32ias/__main__.py
@@ -1,4 +1,26 @@
-from rv32ias import main
+import argparse
+from pathlib import Path
+
+from rv32ias import assembler
+
+
+def main():
+    parser = argparse.ArgumentParser(description='RISC-V RV32I Assembler')
+    parser.add_argument('asm_file', type=str, help='Assembly file to be assembled')
+    args = parser.parse_args()
+
+    asm_file = Path(args.asm_file)
+
+    if not asm_file.exists():
+        print(f'Error: {asm_file} does not exist')
+        exit(1)
+
+    with open(asm_file, 'r') as f:
+        asm_txt = f.read()
+
+    for i, machine_code in enumerate(assembler.assemble(asm_txt)):
+        print(f'{i * 4:04x}: {machine_code:08x}')
+
 
 if __name__ == '__main__':
-    main.main()
+    main()
