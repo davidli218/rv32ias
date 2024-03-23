ce6e3e36c21318c45590c84669f22c9d96019008
Ri Teii <teiiri@outlook.com>
2024-03-23 16:06:39 +0000

    🐞 Fix example assembly code

--- example.asm
+++ example.asm
@@ -1,13 +1,13 @@
 # sum of 0 to 9
 
-    addi s0, zero, 0     # sum = 0
-    addi s1, zero, 10    # n = 10
+    addi s1, zero, 0     # sum = 0
+    addi s2, zero, 10    # n = 10
     addi t0, zero, 0     # counter = 0
 
 # loop start
-    slt  t1, s1,   t0    # if (n - counter) == 0
-    beq  t1, zero, 16    #     break;
-    add  s0, s0,   t0    # sum += counter
+    slt  t1, t0,   s2    # comp_result = n < counter
+    beq  t1, zero, 16    # if comp_result == 0, brake loop
+    add  s1, s1,   t0    # sum += counter
     addi t0, t0,   1     # counter++;
     jal  t2, -16         # continue;
 # loop end
