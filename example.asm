73a8dbe0b1ce43c21a8bf63dd1ead6dfe437f174
Ri Teii <teiiri@outlook.com>
2024-03-25 22:04:05 +0000

    🛠️ Update example assembly code

--- example.asm
+++ example.asm
@@ -4,12 +4,12 @@
     addi s2, zero, 10    # n = 10
     addi t0, zero, 0     # counter = 0
 
-# loop start
+loop:
     slt  t1, t0,   s2    # comp_result = n < counter
-    beq  t1, zero, 16    # if comp_result == 0, brake loop
+    beq  t1, zero, exit  # if comp_result == 0, brake loop
     add  s1, s1,   t0    # sum += counter
     addi t0, t0,   1     # counter++;
-    jal  t2, -16         # continue;
-# loop end
+    jal  t2, loop        # continue;
 
+exit:
     jal  t2, 0           # exit program
