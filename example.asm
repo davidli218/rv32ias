0b814ac106ae234f93a8c4e9956ed9c2867b731d
Ri Teii <teiiri@outlook.com>
2024-03-28 11:43:32 +0000

    🐞 Fix example assembly code

--- example.asm
+++ example.asm
@@ -8,8 +8,8 @@ loop:
     slt  t1, t0,   s2    # comp_result = n < counter
     beq  t1, zero, exit  # if comp_result == 0, brake loop
     add  s1, s1,   t0    # sum += counter
-    addi t0, t0,   1     # counter++;
-    jal  t2, loop        # continue;
+    addi t0, t0,   1     # counter++
+    jal  t2, loop        # continue
 
 exit:
-    jal  t2, 0           # exit program
+    jal  t2, exit        # exit
