874059263bcec9bf97069855cfc6e43d61b2e070
Ri Teii <teiiri@outlook.com>
2024-04-27 02:13:05 +0100

    🐞 Fix color assignment for `AsmLine`

--- rv32ias/models.py
+++ rv32ias/models.py
@@ -49,16 +49,17 @@ class AsmLine:
                 c = self.raw[self.body_offset + len(self.body):]
                 return f'\033[45m{a}\033[47m{b}\033[41m{c}\033[0m'
             case AsmLineType.LABEL | AsmLineType.INSTRUCTION:
+                color = '\033[44m' if self.type == AsmLineType.LABEL else '\033[42m'
                 a = self.raw[:self.body_offset]
                 b = self.raw[self.body_offset:self.body_offset + len(self.body)]
                 if self.tc:
                     c = self.raw[self.body_offset + len(self.body):self.tc_offset]
                     d = self.raw[self.tc_offset:self.tc_offset + len(self.tc)]
                     e = self.raw[self.tc_offset + len(self.tc):]
-                    return f'\033[45m{a}\033[44m{b}\033[45m{c}\033[47m{d}\033[41m{e}\033[0m'
+                    return f'\033[45m{a}{color}{b}\033[45m{c}\033[47m{d}\033[41m{e}\033[0m'
                 else:
                     c = self.raw[self.body_offset + len(self.body):]
-                    return f'\033[45m{a}\033[44m{b}\033[41m{c}\033[0m'
+                    return f'\033[45m{a}{color}{b}\033[41m{c}\033[0m'
 
     def __str__(self):
         return self.raw
